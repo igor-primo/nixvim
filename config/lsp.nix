d2a5798530e17f5ca6ac386ef68c34cec9a3ef32
Igor Primo <igorprimo62@gmail.com>
2026-02-27 20:19:20 -0300

    hls importlens

--- config/lsp.nix
+++ config/lsp.nix
@@ -8,27 +8,30 @@
           enable = true;
           package = null;
           installGhc = false;
-          settings = {
-            haskell = {
-              plugin = {
-                importLens = {
-                  codeActionsOn = false;
-                  codeLensOn = true;
-                };
-              };
-            };
-          };
         };
         html.enable = true;
         cssls.enable = true;
       };
       inlayHints = true;
       onAttach = ''
-        -- Disable LSP features for non-file URI schemes (e.g. fugitive://)
+        -- Disable LSP features for non-file URI scheme (e.g. fugitive://)
         if vim.uri_from_bufnr(bufnr):sub(1, 4) ~= "file" then
           client.server_capabilities.inlayHintProvider = false
         end
       '';
     };
   };
+
+  extraConfigLua = ''
+    -- Suppress noisy HLS importLens notification (cosmetic bug in HLS)
+    local orig_show_message = vim.lsp.handlers["window/showMessage"]
+    vim.lsp.handlers["window/showMessage"] = function(err, result, ctx, config)
+      if result and result.message and result.message:find("importLens: Rule Failed") then
+        return
+      end
+      if orig_show_message then
+        orig_show_message(err, result, ctx, config)
+      end
+    end
+  '';
 }
