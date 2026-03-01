fe6da1c565f709733c6b8e6a676b97ca9bb56ea8
Igor Primo <igorprimo62@gmail.com>
2026-03-01 15:58:10 -0300

    adjust formatter timeout and indexing issue

--- config/conform.nix
+++ config/conform.nix
@@ -11,15 +11,15 @@
             end
 
             local function on_format(err)
-              if err and err:match("timeout$") then
+              if err and err:match("timeout$") and slow_format_filetypes then
                 slow_format_filetypes[vim.bo[bufnr].filetype] = true
               end
             end
 
-            return { timeout_ms = 200, lsp_fallback = true }, on_format
+            return { timeout_ms = 3000, lsp_fallback = true }, on_format
            end
         '';
-        format_on_save_timeout = 1000;
+        format_on_save_timeout = 3000;
         formatters_by_ft = {
           nix = [ "nixfmt" ];
           haskell = [ "fourmolu" ];
