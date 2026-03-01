b5d6ea77f27e5be05bcc667b016301f09b8cd63d
Igor Primo <igorprimo62@gmail.com>
2026-03-01 17:17:21 -0300

    extract keymaps to its own file

---
+++ config/keymaps.nix
@@ -0,0 +1,149 @@
+{
+  keymaps = [
+    {
+      action = ":Neotree toggle<CR>";
+      key = "<Space>e";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      action = ":w<CR>";
+      key = "<C-s>";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      action = ":messages<CR>";
+      key = "<leader>m";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "s";
+      action = "<cmd>lua require('flash').jump()<CR>";
+      options = {
+        silent = true;
+        desc = "Flash jump";
+      };
+    }
+    {
+      key = "<leader>g";
+      action = ":Neogit<CR>";
+      options = {
+        silent = true;
+        desc = "Neogit";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gs";
+      action = ":Git<CR>";
+      options = {
+        silent = true;
+        desc = "Git status";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gc";
+      action = ":Git commit<CR>";
+      options = {
+        silent = true;
+        desc = "Git commit";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gP";
+      action = ":Git push<CR>";
+      options = {
+        silent = true;
+        desc = "Git push";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gp";
+      action = ":Git pull<CR>";
+      options = {
+        silent = true;
+        desc = "Git pull";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gd";
+      action = ":Gdiffsplit<CR>";
+      options = {
+        silent = true;
+        desc = "Git diff split";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = ":Git blame<CR>";
+      options = {
+        silent = true;
+        desc = "Git blame";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gl";
+      action = ":Git log --oneline<CR>";
+      options = {
+        silent = true;
+        desc = "Git log";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gB";
+      action = ":GBrowse<CR>";
+      options = {
+        silent = true;
+        desc = "Git browse";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tl";
+      action = ":colorscheme github_light_high_contrast<CR>";
+      options = {
+        silent = true;
+        desc = "GitHub light high contrast theme";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>td";
+      action = ":colorscheme github_dark_high_contrast<CR>";
+      options = {
+        silent = true;
+        desc = "GitHub dark high contrast theme";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>g/";
+      action.__raw = ''
+        function()
+          vim.ui.input({ prompt = "Git log -S (pickaxe): " }, function(input)
+            if input and input ~= "" then
+              vim.cmd("Git log -S" .. vim.fn.shellescape(input) .. " --oneline")
+            end
+          end)
+        end
+      '';
+      options = {
+        silent = true;
+        desc = "Git pickaxe search";
+      };
+    }
+  ];
+}
