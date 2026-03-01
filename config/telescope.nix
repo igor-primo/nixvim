bc40dd99ebe8cfae4ccf3b3a843bd1a4b7321ac2
Igor Primo <igorprimo62@gmail.com>
2026-03-01 12:57:47 -0300

    fix indentation

--- config/telescope.nix
+++ config/telescope.nix
@@ -4,21 +4,21 @@
       enable = true;
       autoLoad = true;
       extensions = {
-	fzf-native = {
-	  enable = true;
-	};
+        fzf-native = {
+          enable = true;
+        };
       };
       keymaps = {
         "<C-p>" = {
-	  action = "git_files";
-	    options = {
-	      desc = "Telescope Git Files";
-	    };
-	};
-	"<leader>fg" = "live_grep";
-	"<leader>fb" = "buffers";
-	"<leader>fh" = "help_tags";
-	"<leader>ff" = "find_files";
+          action = "git_files";
+          options = {
+            desc = "Telescope Git Files";
+          };
+        };
+        "<leader>fg" = "live_grep";
+        "<leader>fb" = "buffers";
+        "<leader>fh" = "help_tags";
+        "<leader>ff" = "find_files";
       };
     };
   };
